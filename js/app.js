6573017959ed2c313b94a50572a32f6aaeb73d36
Patty8909 <patty90_30@hotmail.com>
2018-02-20 12:01:09 -0500

    añade imagenes func y estilos

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('#modal1').modal();
     $('#modal2').modal();
     $('#modal3').modal();
+    $('#modal4').modal();
 
     $('.datepicker').pickadate({
       selectMonths: true, 
@@ -14,28 +15,26 @@ $(document).ready(function(){
   });
 
 /*---------agregar imágenes--------------*/
-  function handleFileSelect(evt) {
-    var files = evt.target.files; 
-    for (var i = 0, f; f = files[i]; i++) {
+document.getElementById('fileImage').onchange = function(evt) {
+  var files = evt.target.files; 
+  console.log(files);
+  for (var i = 0, f; f = files[i]; i++) {
 
-      if (!f.type.match('image.*')) {
-        continue;
-      }
-      var reader = new FileReader();
-      reader.onload = (function(theFile) {
-        return function(e) {
-          // Render thumbnail.
-          var span = document.createElement('span');
-          span.innerHTML = ['<img class="thumb" src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/>'].join('');
-          document.getElementById('list').insertBefore(span, null);
-        };
-      })(f);
-      reader.readAsDataURL(f);
+    if (!f.type.match('image.*')) {
+      continue;
     }
+    var reader = new FileReader();
+    reader.onload = (function(theFile) {
+      return function(e) {
+        var paragraph = document.createElement('p');
+        paragraph.innerHTML = ['<img class="thumb" src="', e.target.result,
+                          '" title="', escape(theFile.name), '"/>'].join('');
+        document.getElementById('list').insertBefore(paragraph, null);
+        };
+    })(f);
+    reader.readAsDataURL(f);
   }
-
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+}
 /*------------termina agregar imágenes ------*/
 
 /*-----------agregar mensajes -------------*/
@@ -55,41 +54,3 @@ boton.addEventListener('click', function(event) {
 })
 /*-----------termina agregar mensajes -------------*/
 
-/*----------agregar evento------------*/
-var ubic = document.getElementById('mylocation');
-var output = document.getElementById("out");
-var eventoPubli = document.getElementById('eventoPublicar');
-var fechaPubli = document.getElementById('fechaEventoPub');
-var evento = document.getElementById('evento');
-var fechaEvento = document.getElementById('fechaEvento');
-
-ubic.addEventListener('click', function(event) {
-  if (evento.value && fechaEvento.value) {
-    eventoPubli.textContent = evento.value;
-    fechaPubli.textContent = fechaEvento.value;
-  }
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocalización no soporta el navegador</p>";
-    return;
-  }
-
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
-    var img = new Image();
-    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    output.appendChild(img);
-  };
-
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  };
-
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);
-})
